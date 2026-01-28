4c2149388bd7e7a341d665947550f7904f5ee9d2
himanshu110011 <its.hu1910@gmail.com>
2026-01-28 14:44:17 +0530

    Time: 1 ms (99.85%), Space: 47.1 MB (38.56%) - LeetHub

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -1,14 +1,11 @@
 class Solution {
-    public int singleNumber(int[] arr) {
-       int n=arr.length;
-       int m=0;
-       for(int i:arr){
-        int c=0;
-        for(int j:arr){
-            if(i==j) c++;
+    public int singleNumber(int[] nums) {
+        int xor = 0;
+
+        for (int n : nums) {
+            xor = xor ^ n;
         }
-        if(c==1) m=i;
-       }
-       return m;
+
+        return xor;
     }
 }
