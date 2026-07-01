923e2f3f6439ea3eb597ca82ed5bdda526a8bb7e
himanshu110011 <its.hu1910@gmail.com>
2026-07-01 21:44:56 +0530

    Time: 1 ms (99.96%), Space: 77 MB (92.03%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -1,31 +1,31 @@
 class Solution {
     public int maxSubArray(int[] arr) {
-    //    int maxSum = arr[0];
-    //     int currentSum = arr[0];
+       int maxSum = arr[0];
+        int currentSum = arr[0];
 
-    //     for (int i = 1; i < arr.length; i++) {
-    //         currentSum = Math.max(arr[i], currentSum + arr[i]);
-    //         maxSum = Math.max(maxSum, currentSum);
-    //     }
-    //    return maxSum;
+        for (int i = 1; i < arr.length; i++) {
+            currentSum = Math.max(arr[i], currentSum + arr[i]);
+            maxSum = Math.max(maxSum, currentSum);
+        }
+       return maxSum;
 
-    int maxSum = Integer.MIN_VALUE;
-    int currentSum = 0;
+    // int maxSum = Integer.MIN_VALUE;
+    // int currentSum = 0;
     
-    int maxEle = Integer.MIN_VALUE;
-    for(int i : arr){
-        maxEle = Math.max(maxEle, i);
-    }
+    // int maxEle = Integer.MIN_VALUE;
+    // for(int i : arr){
+    //     maxEle = Math.max(maxEle, i);
+    // }
 
-    if(maxEle < 0){
-        return maxEle;
-    }
+    // if(maxEle < 0){
+    //     return maxEle;
+    // }
 
-    for(int i=0; i<arr.length; i++){
-        currentSum += arr[i];
-        if(currentSum < 0) currentSum = 0;
-        if(currentSum > maxSum)  maxSum = currentSum;
-    }
-    return maxSum;
+    // for(int i=0; i<arr.length; i++){
+    //     currentSum += arr[i];
+    //     if(currentSum < 0) currentSum = 0;
+    //     if(currentSum > maxSum)  maxSum = currentSum;
+    // }
+    // return maxSum;
     }
 }
