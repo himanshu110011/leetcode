8bd19acb9ce3325e25ee2d40f2cec1e27c6eefd4
himanshu110011 <its.hu1910@gmail.com>
2026-08-13 11:11:11 +0530

    Time: 1 ms (95.86%), Space: 46.6 MB (91.84%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.java
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.java
@@ -1,35 +1,37 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 class Solution {
     public List<List<Integer>> levelOrder(TreeNode root) {
+
         List<List<Integer>> list = new ArrayList<>();
-        if(root == null) return list;
-        Queue <TreeNode> que = new ArrayDeque<>();
+
+        if (root == null) return list;
+
+        Queue<TreeNode> que = new LinkedList<>();
+
         que.offer(root);
-        while(!que.isEmpty()){
-            int size = que.size();
-            List<Integer> l = new ArrayList<>();
-            for(int i=1; i<=size; i++){
-                TreeNode curr = que.poll();
+        que.offer(null);
+
+        List<Integer> l = new ArrayList<>();
+
+        while (!que.isEmpty()) {
+
+            TreeNode curr = que.poll();
+            if (curr == null) {
+                list.add(l);
+                l = new ArrayList<>();
+                if (!que.isEmpty()) {
+                    que.offer(null);
+                }
+            } else {
                 l.add(curr.val);
-                if(curr.left != null) que.offer(curr.left);
-                if(curr.right != null) que.offer(curr.right);
+                if (curr.left != null) {
+                    que.offer(curr.left);
+                }
+                if (curr.right != null) {
+                    que.offer(curr.right);
+                }
             }
-            list.add(l);
         }
+
         return list;
     }
 }
