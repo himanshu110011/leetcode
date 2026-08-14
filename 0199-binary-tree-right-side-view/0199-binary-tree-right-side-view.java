a5e85d8feffb9a54f4881ee7be1f182f397d4078
himanshu110011 <its.hu1910@gmail.com>
2026-08-14 11:42:09 +0530

    Time: 1 ms (69.9%), Space: 43.5 MB (84.93%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.java
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.java
@@ -1,25 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
     public List<Integer> rightSideView(TreeNode root) {
         List<Integer> list = new ArrayList<>();
-
-        if (root == null) return list;
-
-        Queue<TreeNode> que = new ArrayDeque<>();
+        if(root == null) return list;
+        Queue <TreeNode> que = new ArrayDeque<>();
         que.offer(root);
-
-        while (!que.isEmpty()) {
+        while(!que.isEmpty()){
             int size = que.size();
-            for (int i = 1; i <= size; i++) {
+            for(int i=1; i<=size; i++){
                 TreeNode curr = que.poll();
-                if (i == size) {
-                    list.add(curr.val);
-                }
-                if (curr.left != null) {
-                    que.offer(curr.left);
-                }
-                if (curr.right != null) {
-                    que.offer(curr.right);
-                }
+                if(i == size) list.add(curr.val);
+                if(curr.left != null) que.offer(curr.left);
+                if(curr.right != null) que.offer(curr.right);
             }
         }
         return list;
