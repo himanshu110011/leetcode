46387f1d01b298cf4be253ac0272963cbc9438ff
himanshu110011 <its.hu1910@gmail.com>
2026-08-14 22:54:15 +0530

    Time: 3 ms (22.69%), Space: 45.1 MB (97.07%) - LeetHub

--- 0106-construct-binary-tree-from-inorder-and-postorder-traversal/0106-construct-binary-tree-from-inorder-and-postorder-traversal.java
+++ 0106-construct-binary-tree-from-inorder-and-postorder-traversal/0106-construct-binary-tree-from-inorder-and-postorder-traversal.java
@@ -14,10 +14,10 @@
  * }
  */
 class Solution {
-    int postIdx ;
+    int postIdx;
     public TreeNode buildTree(int[] inorder, int[] postorder) {
-        postIdx = postorder.length-1;
-        return helper( inorder, postorder, 0, inorder.length-1);
+        postIdx = inorder.length-1;
+        return helper(inorder, postorder, 0, inorder.length-1);
     }
     public TreeNode helper(int[] inorder, int[] postorder, int left, int right){
         if(left > right) return null;
@@ -25,13 +25,12 @@ class Solution {
         int inIdx = search(inorder, left, right, postorder[postIdx]);
         postIdx--;
         root.right = helper(inorder, postorder, inIdx+1, right);
-        root.left = helper(inorder, postorder,left, inIdx-1);
-        
+        root.left = helper(inorder, postorder, left, inIdx-1);
         return root;
     }
     public int search(int[] inorder, int left, int right, int val){
         for(int i=left; i<=right; i++){
-            if(inorder[i] == val)return i;
+             if(inorder[i] == val)return i;
         }
         return -1;
     }
