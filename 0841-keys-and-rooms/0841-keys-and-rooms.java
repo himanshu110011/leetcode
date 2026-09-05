8ddbeeecedc3670ad7929afaf9f914680af2654a
himanshu110011 <its.hu1910@gmail.com>
2026-09-05 23:29:36 +0530

    Time: 2 ms (47.94%), Space: 46.3 MB (45.07%) - LeetHub

--- 0841-keys-and-rooms/0841-keys-and-rooms.java
+++ 0841-keys-and-rooms/0841-keys-and-rooms.java
@@ -1,21 +1,24 @@
 class Solution {
     public boolean canVisitAllRooms(List<List<Integer>> rooms) {
+        Queue<Integer> que =  new ArrayDeque<>();
         boolean vis[] = new boolean[rooms.size()];
-        
-        dfs(rooms, vis, 0);
+
+        vis[0] = true;
+        que.offer(0);
+
+        while(!que.isEmpty()){
+            int curr = que.poll();
+
+            for(int key : rooms.get(curr)){
+                if(!vis[key]){
+                    vis[key] = true;
+                    que.offer(key);
+                }
+            }
+        }
         for(boolean v : vis){
             if(!v) return false;
         }
-        
         return true;
-    }
-
-    public void dfs(List<List<Integer>> rooms, boolean[] vis, int room){
-        if(vis[room]) return;
-        vis[room] = true;
-
-        for(int key : rooms.get(room)){
-            dfs(rooms, vis, key);
-        }
     }
 }
