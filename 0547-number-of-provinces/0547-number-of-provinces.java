195a2d539ef7223d7f067985f6f51413c3bc5da2
himanshu110011 <its.hu1910@gmail.com>
2026-09-06 21:50:17 +0530

    Time: 5 ms (8.42%), Space: 49.8 MB (6.66%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.java
+++ 0547-number-of-provinces/0547-number-of-provinces.java
@@ -2,11 +2,11 @@ class Solution {
     public int findCircleNum(int[][] isConnected) {
         int n = isConnected.length;
         ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
-        for(int i=0; i<isConnected.length; i++){
+        for (int i = 0; i < isConnected.length; i++) {
             adj.add(new ArrayList<>());
         }
-        for(int i=0; i<isConnected.length; i++){
-            for(int j=0; j<isConnected[0].length; j++){
+        for (int i = 0; i < isConnected.length; i++) {
+            for (int j = 0; j < isConnected[0].length; j++) {
                 if (isConnected[i][j] == 1) {
                     adj.get(i).add(j);
                 }
@@ -16,16 +16,17 @@ class Solution {
         boolean[] vis = new boolean[n];
         Queue<Integer> que = new ArrayDeque<>();
         int provinces = 0;
-        for(int i=0; i<n; i++){
-            if(!vis[i]){
-              provinces++;
-              que.offer(i);
+        for (int i = 0; i < n; i++) {
+            if (!vis[i]) {
+                provinces++;
+                vis[i] = true;
+                que.offer(i);
             }
-            while(!que.isEmpty()){
+            while (!que.isEmpty()) {
                 int curr = que.poll();
 
-                for(int nbr : adj.get(curr)){
-                    if(!vis[nbr]){
+                for (int nbr : adj.get(curr)) {
+                    if (!vis[nbr]) {
                         vis[nbr] = true;
                         que.offer(nbr);
                     }
