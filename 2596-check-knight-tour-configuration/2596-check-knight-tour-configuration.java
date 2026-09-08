419b2b87ff5c3d3a9dc4e7a1bec9bed6b5da0590
himanshu110011 <its.hu1910@gmail.com>
2026-09-08 11:00:53 +0530

    Time: 1 ms (96.05%), Space: 46.2 MB (84.84%) - LeetHub

--- 2596-check-knight-tour-configuration/2596-check-knight-tour-configuration.java
+++ 2596-check-knight-tour-configuration/2596-check-knight-tour-configuration.java
@@ -2,36 +2,32 @@ class Solution {
     public boolean checkValidGrid(int[][] grid) {
         int n = grid.length;
 
-        if (grid[0][0] != 0)
-            return false;
+        if(grid[0][0] != 0) return false;
 
         return solve(grid, 0, 0, 1);
     }
-
-    public boolean solve(int[][] grid, int r, int c, int num) {
+    public boolean solve(int[][] grid, int r, int c, int num){
         int n = grid.length;
 
-        if (num == n * n) {
-            return true;
-        }
-        int[][] moves = {
-                { 1, 2 },
-                { 1, -2 },
-                { -1, 2 },
-                { -1, -2 },
-                { 2, 1 },
-                { 2, -1 },
-                { -2, 1 },
-                { -2, -1 }
+        if(num == n * n) return true;
+
+        int moves[][] = {
+            {1, 2},
+            {1, -2},
+            {-1, 2},
+            {-1, -2},
+            {2, 1},
+            {2, -1},
+            {-2, 1},
+            {-2, -1}
         };
-        for (int[] move : moves) {
+
+        for(int[] move : moves){
             int nr = r + move[0];
             int nc = c + move[1];
 
-            if (nr >= 0 && nr < n && nc >= 0 && nc < n && grid[nr][nc] == num) {
-                if (solve(grid, nr, nc, num + 1)) {
-                    return true;
-                }
+            if(nr >= 0 && nr < n && nc >= 0 && nc < n && grid[nr][nc] == num){
+                if(solve(grid, nr, nc, num + 1)) return true;
             }
         }
         return false;
