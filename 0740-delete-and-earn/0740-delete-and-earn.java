095e16dba5cff7370209fecab490618bf1a224e2
himanshu110011 <its.hu1910@gmail.com>
2026-09-15 13:41:22 +0530

    Time: 3 ms (73.8%), Space: 47.2 MB (17.92%) - LeetHub

--- 0740-delete-and-earn/0740-delete-and-earn.java
+++ 0740-delete-and-earn/0740-delete-and-earn.java
@@ -1,33 +1,32 @@
 class Solution {
     public int deleteAndEarn(int[] nums) {
-
         int max = 0;
-
-        for (int num : nums) {
-            max = Math.max(max, num);
+        for(int num : nums){
+           max = Math.max(max, num);
         }
-
         int[] sum = new int[max + 1];
 
-        for (int num : nums) {
+        for(int num : nums){
             sum[num] += num;
         }
 
-        int[] dp = new int[max + 1];
+        int dp[] = new int[max + 1];
 
-        dp[0] = 0;
+        Arrays.fill(dp, -1);
 
-        if (max >= 1) {
-            dp[1] = sum[1];
-        }
+        return solve(max, dp, sum);
+    }
+    public int solve(int i, int[] dp, int[] sum){
+        if(i <= 0) return 0;
 
-        for (int i = 2; i <= max; i++) {
-            dp[i] = Math.max(
-                dp[i - 1],
-                sum[i] + dp[i - 2]
-            );
-        }
+        if(dp[i] != -1) return dp[i];
 
-        return dp[max];
+        int notTake = solve(i - 1, dp, sum);
+
+        int take = sum[i] + solve(i - 2, dp, sum);
+
+        dp[i] = Math.max(take, notTake);
+
+        return dp[i];
     }
 }
